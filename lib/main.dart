37a67a854ae14f77560ffa093c8d1b10c25796ff
Abey Joseph <abeyna567@gmail.com>
2025-02-21 03:40:02 +0800

    init

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(TrackApp());
+}
+
+class TrackApp extends StatelessWidget {
+  const TrackApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
